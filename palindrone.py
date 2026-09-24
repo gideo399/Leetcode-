5424c364dc702064707ff6ef820ba9ad0b8924e4
Gideon Tetteh Nyamedi <nyamedigideon1@gmail.com>
2026-09-24 22:16:58 +0000

    palindrone for numbers on leetcode

---
+++ palindrone.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        n = str(x)
+        if n == n[::-1] :
+            return True
+
+        else:
+            return False 
+        
